e3de78c8a2f09a3605efc0006045557188b86e46
mmmpa <mmmpa.mmmpa@gmail.com>
2018-02-23 02:58:07 +0900

    :recycle: refactored

--- main.go
+++ main.go
@@ -11,47 +11,56 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Notifications struct {
-	Items []Notification
+type Attachment struct {
+	Color      string `json:"color"`
+	AuthorName string `json:"author_name"`
+	AuthorLink string `json:"author_link"`
+	Title      string `json:"title"`
+	TitleLink  string `json:"title_link"`
+	Text       string `json:"text"`
+	ThumbURL   string `json:"thumb_url"`
+}
+
+type Mention struct {
+	Body    string `json:"body"`
+	HTMLURL string `json:"html_url"`
+	User struct {
+		Login     string `json:"login"`
+		URL       string `json:"url"`
+		AvatarURL string `json:"avatar_url"`
+	} `json:"user"`
 }
 
 type Notification struct {
 	Reason string `json:"reason"`
 	Subject struct {
 		LatestCommentURL string `json:"latest_comment_url"`
-		Title            string `json:"title"`
-		Type             string `json:"type"`
-		URL              string `json:"url"`
 	} `json:"subject"`
 }
 
-type Mention struct {
-	Body string `json:"body"`
-	User struct {
-		Login string `json:"login"`
-	} `json:"user"`
+func (n Notification) IsMention() bool {
+	return n.Reason == "mention"
 }
 
 type Payload struct {
-	Text string `json:"text"`
+	Text        string       `json:"text"`
+	Attachments []Attachment `json:"attachments"`
 }
 
 func main() {
-	lambda.Start(Handler)
+	if len(os.Getenv("LOCAL_RUN")) > 0 {
+		execute()
+	} else {
+		lambda.Start(execute)
+	}
 }
 
-func Handler() {
-	now := time.Now().UTC()
-
-	api := fmt.Sprintf(
-		"https://api.github.com/notifications?participating=true&since=%s&before=%s",
-		now.Add(time.Duration(-1) * time.Hour).Format("2006-01-02T15")+":00:00Z",
-		now.Format("2006-01-02T15")+":00:00Z",
-	)
+func execute() {
+	api := "https://api.github.com/notifications?participating=true"
 	token := os.Getenv("KICK_MY_MENTION_TOKEN")
 	hook := os.Getenv("KICK_MY_MENTION_SLACK_HOOK")
 
-	err, notifications := fetchNotifications(api, token)
+	notifications, err := fetchNotifications(api, token)
 
 	if err != nil {
 		fmt.Errorf("%v", err)
@@ -59,43 +68,95 @@ func Handler() {
 	}
 
 	for _, notification := range notifications {
-		err, mention := fetchMention(notification.Subject.LatestCommentURL)
-		if err == nil {
-			err = postMessage(hook, Payload{
-				Text: fmt.Sprintf(
-					"from: %v, message: %v, url: %v",
-					mention.User.Login,
-					mention.Body,
-					notification.Subject.LatestCommentURL,
-				),
-			})
+		if !notification.IsMention() {
+			continue
+		}
+
+		mention, err := fetchMention(notification.Subject.LatestCommentURL)
+
+		if err != nil {
 			fmt.Errorf("%v", err)
-		} else {
+			continue
+		}
+
+		err = postMessage(hook, mention)
+		if err != nil {
 			fmt.Errorf("%v", err)
 		}
 	}
 }
 
-func postMessage(url string, payload Payload) error {
-	payloadString, err := json.Marshal(payload)
+func fetch(url, token string) ([]byte, error) {
+	req, _ := http.NewRequest("GET", url, nil)
+	if len(token) > 0 {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+	client := new(http.Client)
+	resp, err := client.Do(req)
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	req, err := http.NewRequest(
-		"POST",
-		url,
-		bytes.NewBuffer(payloadString),
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+func fetchMention(url string) (Mention, error) {
+	var mention Mention
+
+	b, err := fetch(url, "")
+
+	if err != nil {
+		return mention, err
+	}
+
+	return mention, json.Unmarshal(b, &mention)
+}
+
+func fetchNotifications(url, token string) ([]Notification, error) {
+	var notifications []Notification
+
+	since, before := sinceAndBefore()
+	queried := fmt.Sprintf("%s&since=%s&before=%s", url, since, before)
+	b, err := fetch(queried, token)
+
+	if err != nil {
+		return notifications, err
+	}
+
+	return notifications, json.Unmarshal(b, &notifications)
+}
+
+func postMessage(url string, mention Mention) error {
+	payloadString, err := json.Marshal(
+		Payload{
+			Attachments: []Attachment{
+				{
+					Color:      "#1abc9c",
+					AuthorName: fmt.Sprintf("from @%v", mention.User.Login),
+					AuthorLink: mention.User.URL,
+					Title:      mention.HTMLURL,
+					TitleLink:  mention.HTMLURL,
+					Text:       mention.Body,
+					ThumbURL:   mention.User.AvatarURL,
+				},
+			},
+		},
 	)
 
 	if err != nil {
 		return err
 	}
 
-	// Content-Type 設定
-	req.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadString))
 
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
 	client := &http.Client{}
 	resp, err := client.Do(req)
 
@@ -105,56 +166,13 @@ func postMessage(url string, payload Payload) error {
 
 	defer resp.Body.Close()
 
-	return err
+	return nil
 }
 
-func fetchNotifications(url, token string) (error, []Notification) {
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Authorization", "Bearer "+token)
+func sinceAndBefore() (string, string) {
+	now := time.Now().UTC()
+	since := now.Add(time.Duration(-1) * time.Hour).Format("2006-01-02T15") + ":00:00Z"
+	before := now.Format("2006-01-02T15") + ":00:00Z"
 
-	client := new(http.Client)
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-	if err != nil {
-		return err, nil
-	}
-
-	b, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return err, nil
-	}
-
-	var notifications []Notification
-	err = json.Unmarshal(b, &notifications)
-
-	if err != nil {
-		return err, nil
-	}
-
-	return nil, notifications
-}
-
-func fetchMention(url string) (error, Mention) {
-	var mention Mention
-
-	req, _ := http.NewRequest("GET", url, nil)
-	client := new(http.Client)
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-	if err != nil {
-		return err, mention
-	}
-
-	b, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		return err, mention
-	}
-
-	err = json.Unmarshal(b, &mention)
-
-	return err, mention
+	return since, before
 }
